88eac952ab4dcb65149cd9ee8832c1ec4a4b95af
BengalSapper007 <pmukherjee6905@gmail.com>
2025-12-12 12:17:56 +0530

    orbs and routes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
-import './App.css';
-import { BrowserRouter, Route,Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import About from './components/About';
-import SkillsLanguagesPage from './components/SkillsLanguages';
-import Journey from './components/Journey';
-import GalleryPage from './components/GalleryPage';
-import Contacts from './components/Contact';
-import Home from './components/Home';
-import ScrollToTop from './components/ScrollToTop';
-import Cursor from './components/Cursor';
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/ScrollToTop";
+import Cursor from "./components/Cursor";
+import AnimatedRoutes from "./components/AnimatedRoutes"
+import AnimatedOrbs from "./components/AnimatedOrbs";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <AnimatedOrbs />
         <Cursor />
         <Navbar />
         <ScrollToTop />
-        <Routes>
-          <Route path="/"Component={Home}/>
-          <Route path="/about"Component={About}/>
-          <Route path="/skills"Component={SkillsLanguagesPage}/>
-          <Route path="/journey"Component={Journey}/>
-          <Route path="/gallery"Component={GalleryPage}/>
-          <Route path="/contact"Component={Contacts}/>
-        </Routes>
+        <AnimatedRoutes />
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
